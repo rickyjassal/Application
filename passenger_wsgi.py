f1541842f6b79e0aee485a895fb8893544aa458d
Parminder <parminderjassal@gmail.com>
2026-04-23 15:54:01 +1000

    Add user site-packages for Bluehost Passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,10 @@
 import os
+import site
 import sys
 
 BASE_DIR = os.path.dirname(__file__)
 sys.path.insert(0, BASE_DIR)
+site.addsitedir(os.path.expanduser("~/.local/lib/python3.6/site-packages"))
 
 from dotenv import load_dotenv
 load_dotenv(os.path.join(BASE_DIR, '.env'), override=True)
